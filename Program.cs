25480a52afa9c2e4b63c572a70f4aef08e79879e
David Sanders <david.dj.sanders@gmail.com>
2026-04-01 16:05:08 -0400

    testing init w/ folder moves

--- Program.cs
+++ Program.cs
@@ -8,5 +8,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }); // Register HttpClient for making HTTP requests to the server
 builder.Services.AddScoped<DeathwatchMobile.Services.CharacterStore>(); // Register the CharacterStore service for dependency injection
+builder.Services.AddSingleton<DeathwatchMobile.Services.TalentLibraryService>(); // Register the TalentLibraryService service for dependency injection
 
 await builder.Build().RunAsync();
